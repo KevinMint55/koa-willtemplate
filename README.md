474e9aeb547a45f99d2c16f3ca9999f1e0e326a5
KevinMint55 <innovation55@foxmail.com>
2020-07-22 17:27:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+### 环境准备
+
+- nodejs 10.0.0+
+- mongodb 3.4.9+
+
+### 开发指南
+
+``` bash
+# install dependencies
+npm install / yarn
+
+# run for development
+npm run dev / yarn dev
+
+# run for production
+npm run start / yarn start
+```
---
+++ config/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  port: 5000,
+  database: {
+      USER: 'demo',
+      PASSWORD: '12345678',
+      URL: 'localhost:27017/demo'
+  },
+  secret: 'demo',
+  email: {
+      usrname: 'xxx',
+      user: 'xxx@xxx.com',
+      pass: 'xxx',
+  }
+}
